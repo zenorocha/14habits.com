c4030a73ae3ec78cdd032f3a50e0248dfabd46dc
Zeno Rocha <zno.rocha@gmail.com>
2020-05-20 14:01:23 -0700

    Add dedicated page for Portuguese and English

---
+++ pages/br/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Index from '../../components/Index';
+
+class IndexPortuguese extends React.Component {
+  render() {
+    return <Index locale="pt-BR" />
+  }
+}
+
+export default IndexPortuguese;
--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,10 @@
 import React from 'react';
-import i18n from '../lib/i18n';
+import Index from '../components/Index';
 
-class Index extends React.Component {
+class IndexEnglish extends React.Component {
   render() {
-    return <div className="mt-8 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-20 xl:mt-24">
-      <div className="lg:grid lg:grid-cols-12 lg:gap-8">
-        <div className="sm:text-center md:max-w-1xl md:mx-auto lg:col-span-8 lg:text-left">
-          <div className="text-sm font-semibold uppercase tracking-wide text-gray-500 sm:text-base lg:text-sm xl:text-base">
-            {i18n.t('comingSoon')}
-          </div>
-          <h2 className="mt-1 text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:leading-none sm:text-6xl lg:text-5xl xl:text-6xl">
-            {i18n.t('titleFirstPart')}&nbsp;
-            <br className="hidden md:inline" />
-            <span className="text-indigo-600">{i18n.t('titleSecondPart')}</span>
-          </h2>
-          <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
-            {i18n.t('headerFirstParagraph')}
-          </p>
-          <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
-          	{i18n.t('headerSecondParagraph')}
-          </p>
-          <div className="mt-5 sm:max-w-lg sm:mx-auto sm:text-center lg:text-left lg:mx-0">
-            <p className="text-base font-medium text-gray-900">
-              {i18n.t('notifyCta')}
-            </p>
-            <form action="#" method="POST" className="mt-3 sm:flex">
-              <input aria-label="Email" className="appearance-none block w-full px-3 py-3 border border-gray-300 text-base leading-6 rounded-md placeholder-gray-500 shadow-sm focus:outline-none focus:placeholder-gray-400 focus:shadow-outline focus:border-blue-300 transition duration-150 ease-in-out sm:flex-1" placeholder={i18n.t('enterEmail')} />
-              <button type="submit" className="mt-3 w-full px-6 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 shadow-sm hover:bg-indigo-700 focus:outline-none focus:shadow-outline active:bg-indigo-900 transition duration-150 ease-in-out sm:mt-0 sm:ml-3 sm:flex-shrink-0 sm:inline-flex sm:items-center sm:w-auto">
-                {i18n.t('notifyMe')}
-              </button>
-            </form>
-          </div>
-        </div>
-        <div className="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-4 lg:flex lg:items-center">
-          <img src={i18n.t('bookCover')} alt="Book cover" />
-        </div>
-      </div>
-    </div>
+    return <Index locale="en-US" />
   }
 }
 
-export default Index;
+export default IndexEnglish;
